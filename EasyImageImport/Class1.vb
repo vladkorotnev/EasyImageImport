f0d1db8f1cbf0ee2fff7fdd3d23c1851f7c968f0
Vladislav Korotnev <vladkorotnev@gmail.com>
2011-07-01 22:19:17 +0600

    Initial commit

---
+++ EasyImageImport/Class1.vb
@@ -0,0 +1,84 @@
+﻿
+
+Public Class EasyImageImportClass
+    Public Function UserImportJpgPhotosToFolder(ByVal Folder As String, ByVal FileNameBeforeNumber As String, ByVal DeviceToUse As WIALib.Item)
+        Dim wiaman As WIALib.Wia
+        Dim wiaroot As WIALib.Item
+        wiaman = New WIALib.Wia
+        On Error GoTo nocam
+        wiaroot = DeviceToUse
+        Dim wiapics As WIALib.Collection
+        On Error GoTo nocam
+        wiapics = wiaroot.GetItemsFromUI
+        Dim cnt
+        cnt = 0
+        For cnt = 0 To wiapics.Count
+            Dim wiaitem As WIALib.Item = wiapics.Item(cnt)
+            Dim lastphotonumber As Integer
+            lastphotonumber = My.Computer.FileSystem.GetFiles(Folder).Count
+            On Error GoTo errorka
+            wiaitem.Transfer(Folder + "\" + FileNameBeforeNumber + CStr(lastphotonumber) + ".jpg")
+        Next
+errorka:
+        Return 0
+nocam:
+        Return 2
+unexpected:
+        Return -1
+    End Function
+    Public Function UserImportOnePhoto(ByVal DestPath As String, ByVal DeviceToUse As WIALib.Item) As Integer
+        Dim wiaman As WIALib.Wia
+        Dim wiaroot As WIALib.Item
+        wiaman = New WIALib.Wia
+        wiaroot = DeviceToUse
+        Dim wiapics As WIALib.Collection
+        On Error GoTo nocam
+        wiapics = wiaroot.GetItemsFromUI(WIALib.WiaFlag.SingleImage)
+        Dim cnt
+        cnt = 0
+        For cnt = 0 To wiapics.Count
+            Dim wiaitem As WIALib.Item = wiapics.Item(cnt)
+            Dim lastphotonumber As Integer
+            On Error GoTo errorka
+            wiaitem.Transfer(DestPath)
+        Next
+errorka:
+        Return 0
+nocam:
+        Return 2
+unexpected:
+        Return -1
+    End Function
+
+    Public Function GetDeviceList() As WIALib.Collection
+        Dim wiaman As WIALib.Wia = New WIALib.Wia
+        Return wiaman.Devices
+    End Function
+
+    Public Function GetDevice(ByVal DeviceToGet As Object) As WIALib.Item
+        Dim wiaman As WIALib.Wia = New WIALib.Wia
+        On Error Resume Next
+        Dim dev = wiaman.Create(DeviceToGet)
+        Return dev
+    End Function
+
+    Public Function GetImageListFromUser(ByVal deviceToUse As WIALib.Item) As WIALib.Collection
+        Dim wiaman As WIALib.Wia
+        Dim wiaroot As WIALib.Item
+        wiaman = New WIALib.Wia
+        wiaroot = deviceToUse 'first device
+        Dim wiapics As WIALib.Collection
+        wiapics = wiaroot.GetItemsFromUI(WIALib.WiaFlag.SingleImage)
+        Return wiapics
+    End Function
+
+    Public Function TakeAPicture(ByVal deviceToUse As WIALib.Item)
+        Dim wiaman As WIALib.Wia = New WIALib.Wia
+        Dim wiaroot As WIALib.Item = deviceToUse
+        On Error GoTo Err
+        wiaroot.TakePicture()
+        Return 0
+err:
+        Return -1
+    End Function
+End Class
---
+++ TestEasyPhotoImport/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports EasyImageImport
+Imports WIALib
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If My.Computer.FileSystem.FileExists("C:\foto.jpg") Then
+            My.Computer.FileSystem.DeleteFile("C:\foto.jpg")
+        End If
+        Dim imaimp = New EasyImageImport.EasyImageImportClass
+        Dim device = imaimp.GetDevice(imaimp.GetDeviceList.Item(0)) 'First device
+        Dim resultcode = imaimp.UserImportOnePhoto("C:\foto.jpg", device)
+        If resultcode = 0 Then
+            MsgBox("Library returned 0 -- Success", MsgBoxStyle.Information, "OK")
+        ElseIf resultcode = 2 Then
+            MsgBox("Library returned 2 -- No device", MsgBoxStyle.Information, "No device")
+        ElseIf resultcode = -1 Then
+            MsgBox("Library returned -1 -- unexpected error", MsgBoxStyle.Critical, "Oops!")
+        Else
+            MsgBox("Library returned " + CStr(resultcode) + " -- What the hell???", MsgBoxStyle.Critical, "WTH?")
+        End If
+        If My.Computer.FileSystem.FileExists("C:\foto.jpg") Then
+            MsgBox("Success", MsgBoxStyle.Information, "File exists")
+            My.Computer.FileSystem.DeleteFile("C:\foto.jpg")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Not My.Computer.FileSystem.DirectoryExists("C:\fototest") Then My.Computer.FileSystem.CreateDirectory("C:\fototest")
+        Dim imaimp = New EasyImageImport.EasyImageImportClass
+        Dim device = imaimp.GetDevice(imaimp.GetDeviceList.Item(0)) 'First device
+        Dim resultcode = imaimp.UserImportJpgPhotosToFolder("C:\fototest", "EasyImageImport", device)
+        If resultcode = 0 Then
+            MsgBox("Library returned 0 -- Success", MsgBoxStyle.Information, "OK")
+        ElseIf resultcode = 2 Then
+            MsgBox("Library returned 2 -- No device", MsgBoxStyle.Information, "No device")
+        ElseIf resultcode = -1 Then
+            MsgBox("Library returned -1 -- unexpected error", MsgBoxStyle.Critical, "Oops!")
+        Else
+            MsgBox("Library returned " + CStr(resultcode) + " -- What the hell???", MsgBoxStyle.Critical, "WTH?")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim imaimp As EasyImageImportClass
+        imaimp = New EasyImageImport.EasyImageImportClass
+        Dim list As WIALib.Collection = imaimp.GetDeviceList()
+        Dim i = 0
+        Dim names As String
+
+        For i = 1 To list.Count
+            Dim tmp As WIALib.Item
+            tmp = imaimp.GetDevice(list.Item(i))
+            names = names + vbCrLf + tmp.Name
+            names = names + vbCrLf + "      Firmware:" + tmp.FirmwareVersion
+        Next
+        MsgBox(names, MsgBoxStyle.Information, "Device list")
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim imaimp As EasyImageImport.EasyImageImportClass = New EasyImageImport.EasyImageImportClass
+        Dim resultcode As Integer = imaimp.TakeAPicture(imaimp.GetDevice(imaimp.GetDeviceList.Item(0)))
+        If resultcode = 0 Then
+            MsgBox("Library returned 0 -- success!", MsgBoxStyle.Information, "OK")
+        Else
+            MsgBox("Library returned " + CStr(resultcode), MsgBoxStyle.Critical, "Oops")
+        End If
+    End Sub
+End Class
